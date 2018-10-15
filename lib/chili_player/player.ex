3141cf356bc6b944a84c9b1355c2a8fa0921ba36
Lean <lndr.figueredo@gmail.com>
2018-10-15 19:06:32 -0300

    Updating videos endpoint

--- lib/chili_player/player.ex
+++ lib/chili_player/player.ex
@@ -56,8 +56,23 @@ defmodule ChiliPlayer.Player do
        end).()
   end
 
-  def update(options \\ %{}, headers \\ []) do
+  def update(video_id, options \\ %{}, headers \\ []) do
     params = %{"video" => options}
+    {:ok, body} = Poison.encode params
+    @player_api_url <> to_string(video_id)
+    |> HTTPoison.patch(body, headers)
+    |> case do
+         {:ok, %{body: raw, status_code: code}} -> {code, raw}
+         {:error, %{reason: reason}} -> {:error, reason}
+       end
+    |> (fn {ok, body} ->
+          body
+          |> Poison.decode(keys: :atoms)
+          |> case do
+               {:ok, parsed} -> {ok, parsed}
+               _ -> {:error, body}
+             end
+       end).()
   end
 
   def delete do
