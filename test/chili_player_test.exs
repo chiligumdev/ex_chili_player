34a4fcbc0920e5393df87df1b1343f4d7e3d2f57
Lean <lndr.figueredo@gmail.com>
2019-02-13 23:52:27 -0200

    reorganize tests and adding a travis file

--- test/chili_player_test.exs
+++ test/chili_player_test.exs
@@ -3,14 +3,14 @@ defmodule ChiliPlayerTest do
   doctest Player
 
   setup_all do
-    
+    {:ok, invalid_token: "someinvalidtoken"}
   end
 
-  test "GET videos with invalid token" do
-    assert ChiliPlayer.videos([token: "some_invalid_token"]) == {:error, "Unauthorized - Check your credentials"}
-  end
-
-  test "GET videos with empty params" do
-    assert ChiliPlayer.videos() == {:error, "Unauthorized - Check your credentials"}
+  describe "Try to GET videos with invalid auth paramenters" do
+    test "GET videos with invalid token", state do
+      assert ChiliPlayer.videos([token: state[:invalid_token]]) == {:error, "Unauthorized - Check your credentials"}
+      assert ChiliPlayer.videos() == {:error, "Unauthorized - Check your credentials"}
+      refute ChiliPlayer.videos([token: FigaroElixir.env["token"]]) == {:ok, %{}}
+    end
   end
 end
